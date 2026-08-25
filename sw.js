c741db321e09744fbf5157deef46953bca2f105a
Laxman Nepal <84069546+LaxmanNepal@users.noreply.github.com>
2026-08-25 22:26:35 +0300

    Add lightweight PWA offline shell and runtime caching

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='nisulka-shell-v1';
+const SHELL=['/Nisulka-Tools/','/Nisulka-Tools/index.html','/Nisulka-Tools/assets/css/variables.css','/Nisulka-Tools/assets/css/global.css','/Nisulka-Tools/assets/css/home.css','/Nisulka-Tools/assets/css/home-enhancements.css','/Nisulka-Tools/assets/js/home.js','/Nisulka-Tools/assets/js/home-enhancements.js','/Nisulka-Tools/manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const req=event.request;if(req.method!=='GET')return;event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{if(new URL(req.url).origin===location.origin){const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(req,copy))}return res}).catch(()=>caches.match('/Nisulka-Tools/'))))});
